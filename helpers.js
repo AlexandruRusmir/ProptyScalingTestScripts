314c2064bfcc29267cac848f151a2940b8b05d35
Rusmir Alexandru <rusmiralexandru@gmail.com>
2023-04-13 09:29:06 +0300

    first commit

---
+++ helpers.js
@@ -0,0 +1,33 @@
+const countryList = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua and Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia and Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre and Miquelon","Samoa","San Marino","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","St Kitts and Nevis","St Lucia","St Vincent","St. Lucia","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad and Tobago","Tunisia","Turkey","Turkmenistan","Turks and Caicos","Uganda","Ukraine","United Arab Emirates","United Kingdom","Uruguay","Uzbekistan","Venezuela","Vietnam","Virgin Islands","Yemen","Zambia","Zimbabwe"];
+const titleDeployingContractAddress = '0x1c296fcb195d680a6968c8C0014D575e542Efe7C';
+const accountAddresses = [
+    "0xB22965A60e0482fd1995415B7Fd90bC367F18b7D",
+    "0x4b92CE3674999C79831Bf15EeFcF8c66c20be7B2",
+    "0xeDbd2d92Ec14f5d454d98A88aF8E29a75bE5bCA2",
+    "0x2815272DA37A8aca6094a9E2bCE76aaDE1318159",
+    "0xB0003b51534E5dFdB9e08FFa9b7c5d410caaE4E9",
+    "0xcc96454Dab512A6527996D8bDB94147EfE41b6Cf"
+];
+
+const getRandomCountry = () => {
+    const randomIndex = Math.floor(Math.random() * countryList.length);
+    return countryList[randomIndex];
+}
+
+const getRandomInt = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const getRandomAccountAddress = () => {
+    const randomIndex = Math.floor(Math.random() * accountAddresses.length);
+    return accountAddresses[randomIndex];
+}
+
+module.exports = {
+    titleDeployingContractAddress,
+    getRandomCountry,
+    getRandomInt,
+    getRandomAccountAddress
+}
---
+++ index.js
@@ -0,0 +1,97 @@
+const Web3 = require('web3');
+const fs = require('fs');
+const faker = require('faker');
+const {
+  titleDeployingContractAddress,
+  getRandomCountry,
+  getRandomInt,
+  getRandomAccountAddress
+} = require('./helpers');
+const titleCreatingContractBuild = require('../propty/contracts/build/contracts/TitleCreatingContract.json');
+
+const getRandomCity = () => faker.address.city();
+const getRandomStreet = () => faker.address.streetName();
+
+const web3 = new Web3(Web3.givenProvider || 'http://localhost:8446');
+const titlesContract = new web3.eth.Contract(titleCreatingContractBuild.abi, titleDeployingContractAddress)
+
+const account = '0xB22965A60e0482fd1995415B7Fd90bC367F18b7D';
+const numberOfContracts = 2;
+const results = [];
+const fileToSave = 'results.json';
+
+const getAverageBlockTime = async (numBlocks = 10) => {
+  const latestBlock = await web3.eth.getBlock('latest');
+  const startBlock = Math.max(latestBlock.number - numBlocks, 0);
+
+  let blockTimes = [];
+  for (let i = startBlock; i < latestBlock.number; i++) {
+    const block1 = await web3.eth.getBlock(i);
+    const block2 = await web3.eth.getBlock(i + 1);
+    const timeDifference = block2.timestamp - block1.timestamp;
+    blockTimes.push(timeDifference);
+  }
+
+  const sum = blockTimes.reduce((a, b) => a + b, 0);
+  return sum / blockTimes.length;
+};
+
+(async () => {
+  try {
+    const gasPrice = await web3.eth.getGasPrice();
+
+    for (let i = 0; i < numberOfContracts; i++) {
+      const startTime = Date.now();
+      const averageBlockTime = await getAverageBlockTime();
+      const contractStorageSizeBefore = await web3.eth.getStorageAt(titleDeployingContractAddress, 2);
+      const country = getRandomCountry();
+      const city = getRandomCity();
+      const street = getRandomStreet();
+      const streetNumber = Math.floor(Math.random() * 100);
+      const apartmentNumberToDeploy = Math.floor(Math.random() * 1000);
+      const squareMeters = Math.floor(Math.random() * 200);
+      const sellingPriceIntegralPart = Math.floor(Math.random() * 1000).toString();
+      const sellingPriceFractionalPart = Math.floor(Math.random().toFixed(3) * 1000).toString();
+      const sellingPriceFractionalPartLength = sellingPriceFractionalPart.length;
+
+      const tx = titlesContract.methods.deployNewPropertyTitle(
+        getRandomAccountAddress(),
+        country,
+        city,
+        street,
+        streetNumber,
+        apartmentNumberToDeploy,
+        squareMeters,
+        sellingPriceIntegralPart,
+        sellingPriceFractionalPart,
+        sellingPriceFractionalPartLength
+      ).send({ from: account, gasPrice });
+
+      const transaction = await tx;
+      const endTime = Date.now();
+      const responseTime = (endTime - startTime) / 1000;
+      const contractStorageSizeAfter = await web3.eth.getStorageAt(titleDeployingContractAddress, 2);
+      const storageGrowth = parseInt(contractStorageSizeAfter, 16) - parseInt(contractStorageSizeBefore, 16);
+      const gasUsed = transaction.gasUsed;
+      const spentEther = web3.utils.fromWei(BigInt(gasUsed * gasPrice).toString(), 'ether');
+      const blockNumber = transaction.blockNumber;
+      const block = await web3.eth.getBlock(blockNumber);
+      const blockSize = block.size;
+    
+      results.push({
+        spentEther,
+        averageBlockTime,
+        responseTime,
+        storageGrowth,
+        blockSize
+      });
+    
+      console.log(`Contract ${i + 1} deployed.`);
+    
+      fs.writeFileSync(fileToSave, JSON.stringify(results));
+      console.log(`Results saved to ${fileToSave}`);
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+})();
