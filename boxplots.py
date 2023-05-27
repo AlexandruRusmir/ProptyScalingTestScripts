f3b7e8de38021eebf124a2bfbcdfb6325c3aea0f
Rusmir Alexandru <rusmiralexandru@gmail.com>
2023-05-27 11:25:54 +0300

    enhance boxplots script

--- boxplots.py
+++ boxplots.py
@@ -1,7 +1,11 @@
 import matplotlib.pyplot as plt
 import json
 
-with open('./propertyValidationResults.json', 'r') as f:
+PROPERTY_LAUNCH_RESULTS_FILE_PATH = './results.json'
+PROPERTY_VALIDATION_RESULTS_FILE_PATH = './propertyValidationResults.json'
+
+current_file_path = PROPERTY_LAUNCH_RESULTS_FILE_PATH
+with open(current_file_path, 'r') as f:
     data = json.load(f)
 
 fields = list(data[0].keys())
@@ -10,7 +14,6 @@ box_data = [[entry[field] for entry in data] for field in fields]
 fig, axs = plt.subplots(len(fields), 1, figsize=(10, len(fields)*6))
 
 plt.style.use('seaborn-darkgrid')
-plt.rcParams.update({'axes.titlesize': 'large'})
 
 field_mapping = {
     'spentEther': 'Ether cheltuit',
@@ -19,10 +22,37 @@ field_mapping = {
     'blockSize': 'Dimensiunea blocului'
 }
 
-for i, field in enumerate(fields):
-    axs[i].boxplot(box_data[i], vert=False, patch_artist=True, notch=True, medianprops={'linewidth': 2})
-    axs[i].set_yticklabels([field_mapping[field]], fontsize=14)
-    axs[i].ticklabel_format(style='plain', axis='x', useOffset=False)
+unit_mapping = {
+    'spentEther': '(ETH)',
+    'averageBlockTime': '(secunde)',
+    'responseTime': '(secunde)',
+    'blockSize': '(octeți)'
+}
 
-plt.tight_layout(h_pad=7.0, pad=4.0)
+xlim_mapping = {
+    'spentEther': (0.0021, 0.00217),
+    'blockSize': (10190, 10194)
+} if current_file_path == PROPERTY_LAUNCH_RESULTS_FILE_PATH else {
+    'spentEther': (0.000085, 0.000086),
+    'blockSize': (63040, 63048)
+}
+
+for i, field in enumerate(fields):
+    bplot = axs[i].boxplot(box_data[i], vert=False, patch_artist=True, notch=True, 
+                           flierprops=dict(marker='o', markerfacecolor='gray', markersize=5, linestyle='none'), 
+                           medianprops=dict(linewidth=2))
+
+    bplot['boxes'][0].set_facecolor('lightblue')
+    bplot['boxes'][0].set_edgecolor('gray')
+    axs[i].set_title(field_mapping[field], fontsize=20)
+    axs[i].ticklabel_format(style='plain', axis='x', useOffset=False)
+    axs[i].set_xlabel(unit_mapping[field], fontsize=15)
+    axs[i].tick_params(axis='both', labelsize=12)
+    axs[i].grid(True, linestyle='-', which='major', color='lightgrey', alpha=0.5)
+    axs[i].set_yticklabels([])
+    
+    if field in xlim_mapping:
+        axs[i].set_xlim(xlim_mapping[field])
+
+plt.tight_layout(h_pad=20.0, pad=9.0)
 plt.show()
